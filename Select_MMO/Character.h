a79e7d62ca6c41ff230305b0f512072c70ac2ebf
42hena <somnium1108@gmail.com>
2023-10-22 20:29:02 +0900

    [Feat] 케릭터 구조체
    - 케릭터에 필요한 요소들 넣음.

---
+++ Select_MMO/Character.h
@@ -0,0 +1,30 @@
+#ifndef __CHARACTER_H__
+#define __CHARACTER_H__
+
+#include "Session.h"
+#include "Sector.h"
+
+struct st_Character
+{
+	st_Character(st_Session* newSession, DWORD	newId, BYTE action, BYTE dir, char hp)
+		: session(newSession), characterId(newId), action(action), direction(dir), hp(hp), y(rand() % 6400), x(rand() % 6400), attackTime(0)
+	{
+		sector = { y / 150, x / 150 };
+		prevSector = sector;
+	}
+
+
+	st_Session* session; // 8
+	DWORD		characterId;// 4
+	BYTE		action; // 1
+	BYTE		direction; // 1
+	char		hp;
+	int			y;
+	int			x;
+
+	st_Sector	sector; //4
+	st_Sector	prevSector;// 4
+	DWORD		attackTime;
+};
+
+#endif
