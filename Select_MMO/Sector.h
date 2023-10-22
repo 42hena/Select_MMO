2b4783b5ff3b22d2f6a91b0216ffd010e4404a60
42hena <somnium1108@gmail.com>
2023-10-22 20:29:35 +0900

    [Feat] 섹터에 관한 구조체 구현

---
+++ Select_MMO/Sector.h
@@ -0,0 +1,26 @@
+#ifndef __SECTOR_H__
+#define __SECTOR_H__
+
+struct st_Sector
+{
+	int	sec_y;
+	int sec_x;
+	bool operator==(const st_Sector& copy) const
+	{
+		if (sec_y == copy.sec_y && sec_x == copy.sec_x)
+			return true;
+		return false;
+	}
+	bool operator!=(const st_Sector& copy) const
+	{
+		return !(*this == copy);
+	}
+};
+
+struct st_SECTOR_AROUND
+{
+	int count;
+	st_Sector around[9];
+};
+
+#endif
