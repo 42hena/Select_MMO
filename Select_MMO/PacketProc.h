26fdd0e26a4932f7691026963f7d782045f7a60b
42hena <somnium1108@gmail.com>
2023-10-28 01:43:46 +0900

    [Chore] return type 변경
    - bool에서 void로 변경

--- Select_MMO/PacketProc.h
+++ Select_MMO/PacketProc.h
@@ -1,12 +1,12 @@
 #ifndef __PACKET_PROC_H__
 #define __PACKET_PROC_H__
 
-bool MoveStart(st_Session* session, CSerialization* packet);
-bool MoveStop(st_Session* session, CSerialization* packet);
+void MoveStart(st_Session* session, CSerialization* packet);
+void MoveStop(st_Session* session, CSerialization* packet);
 
-bool Attack1Packet(st_Session* session, CSerialization* buffer);
-//bool Attack2Packet(st_Session* session, CSerialization* buffer);
-//bool Attack3Packet(st_Session* session, CSerialization* buffer);
-bool Echo(st_Session* session, CSerialization* packet);
+void Attack1Packet(st_Session* session, CSerialization* buffer);
+//void Attack2Packet(st_Session* session, CSerialization* buffer);
+//void Attack3Packet(st_Session* session, CSerialization* buffer);
+void Echo(st_Session* session, CSerialization* packet);
 
 #endif
