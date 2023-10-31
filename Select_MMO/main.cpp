91c501cc03e552ba081129d54c8baeda74c45e98
42hena <somnium1108@gmail.com>
2023-10-31 14:57:41 +0900

    [Feat] 전역변수 추가, frame용 시간 추가, Update, Log

--- Select_MMO/main.cpp
+++ Select_MMO/main.cpp
@@ -6,19 +6,24 @@
 #include "Session.h"
 #include "Character.h"
 #include "CNetwork.h"
+#include "Util.h"
 
 std::unordered_map<SOCKET, st_Session*> g_sessionMap;
 std::unordered_map<DWORD, st_Character *> g_characterMap;
 std::list< st_Character* > g_sector[6400 / 150 + 1][6400 / 150 + 1];
-
+DWORD g_sendCnt, g_recvCnt, g_acceptCnt;
+DWORD g_frameTime;
+DWORD totalTime;
 void RunProgram()
 {
 	CNetwork network;
 // -----
-
+	g_frameTime = timeGetTime();
 	while (1)
 	{
 		network.NetworkIO();
+		Update();
+		PrintLog();
 	}
 }
 
